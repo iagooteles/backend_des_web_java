f9a96977a4c3fb48201e603c7688aa185035b0a0
iagooteles <iagooteles@gmail.com>
2025-08-14 22:23:45 -0300

    ..

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,7 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping
 @RestController
 public class DemoController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hiihihihihi";
+    }
 }
